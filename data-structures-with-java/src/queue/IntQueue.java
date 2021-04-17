27482ea5f6412e3a5b23845cfdca24577871809c
ragdoll <museopkim0214@gmail.com>
2021-04-17 22:30:00 +0900

    Stack, Queue

---
+++ data-structures-with-java/src/queue/IntQueue.java
@@ -0,0 +1,105 @@
+package queue;
+
+public class IntQueue {
+
+    private int max;
+    private int front;
+    private int rear;
+    private int count;
+    private int[] queue;
+
+    public class EmptyQueueException extends RuntimeException {
+        public EmptyQueueException() {}
+    }
+
+    public class OverflowQueueException extends RuntimeException {
+        public OverflowQueueException() {}
+    }
+
+    public IntQueue(int capacity) {
+        count = front = rear = 0;
+        max = capacity;
+        try {
+            queue = new int[max];
+        } catch (OutOfMemoryError error) {
+            max = 0;
+        }
+    }
+
+    public int enqueue(int element) throws OverflowQueueException {
+        if (count >= max) {
+            throw new OverflowQueueException();
+        }
+        queue[rear++] = element;
+        count++;
+
+        if (rear == max) {
+            rear = 0;
+        }
+        return element;
+    }
+
+    public int dequeue() throws EmptyQueueException {
+        if (count <= 0) {
+            throw new EmptyQueueException();
+        }
+
+        int element = queue[front++];
+        count--;
+
+        if (front == max) {
+            front = 0;
+        }
+
+        return element;
+    }
+
+    public int peek() throws EmptyQueueException {
+        if (count <= 0) {
+            throw new EmptyQueueException();
+        }
+        return queue[front];
+    }
+
+    public int indexOf(int target) {
+        for (int i = 0; i < count; i++) {
+            int index = (i + front) % max;
+            if (queue[index] == target) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public void clear() {
+        count = front = rear = 0;
+    }
+
+    public int capacity() {
+        return max;
+    }
+
+    public int size() {
+        return count;
+    }
+
+    public boolean isEmpty() {
+        return count <= 0;
+    }
+
+    public boolean isFull() {
+        return count >= max;
+    }
+
+    public void dump() {
+        if (count <= 0) {
+            System.out.println("Queue가 비어 있습니다.");
+            return;
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(queue[(i + front) % max] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ data-structures-with-java/src/stack/IntStack.java
@@ -0,0 +1,88 @@
+package stack;
+
+public class IntStack {
+
+    private int max;
+    private int pointer;
+    private int[] stack;
+
+    public class EmptyStackException extends RuntimeException {
+        public EmptyStackException() {}
+    }
+
+    public class OverflowStackException extends RuntimeException {
+        public OverflowStackException() {}
+    }
+
+    public IntStack(int capacity) {
+        pointer = 0;
+        max = capacity;
+        try {
+            stack = new int[max];
+        } catch (OutOfMemoryError error) {
+            max = 0;
+        }
+    }
+
+    public int push(int element) throws OverflowStackException {
+        if (pointer >= max) {
+            throw new OverflowStackException();
+        }
+        return stack[pointer++] = element;
+    }
+
+    public int pop() throws EmptyStackException {
+        if (pointer <= 0) {
+            throw new EmptyStackException();
+        }
+        return stack[--pointer];
+    }
+
+    public int peek() throws EmptyStackException {
+        if (pointer <= 0) {
+            throw new EmptyStackException();
+        }
+        return stack[pointer - 1];
+    }
+
+    public int indexOf(int target) {
+        for (int i = pointer - 1; i >= 0; i--) {
+            if (stack[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void clear() {
+        pointer = 0;
+    }
+
+    public int capacity() {
+        return max;
+    }
+
+    public int size() {
+        return pointer;
+    }
+
+    public boolean isEmpty() {
+        return pointer <= 0;
+    }
+
+    public boolean isFull() {
+        return pointer >= max;
+    }
+
+    public void dump() {
+        if (pointer <= 0) {
+            System.out.println("Stack이 비어 있습니다.");
+            return;
+        }
+
+        for (int i = 0; i < pointer; i++) {
+            System.out.println(stack[i] + " ");
+        }
+        System.out.println();
+    }
+}

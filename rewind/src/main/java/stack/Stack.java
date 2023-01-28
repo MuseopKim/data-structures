9ea881cef7592943cc913af2f055919a707eee54
Museop <museopkim0214@gmail.com>
2023-01-28 18:54:09 +0900

    feat: stack

--- rewind/src/main/java/stack/Stack.java
+++ rewind/src/main/java/stack/Stack.java
@@ -1,2 +1,33 @@
-package stack;public class Stack {
+package stack;
+
+import linkedlist.LinkedList;
+import linkedlist.LinkedList.Node;
+
+public class Stack<T> {
+
+    private LinkedList<T> linkedList;
+
+    public Stack(LinkedList<T> linkedList) {
+        this.linkedList = linkedList;
+    }
+
+    public void push(T data) {
+        this.linkedList.insertAt(0, data);
+    }
+
+    public Node<T> pop() {
+        try {
+            return this.linkedList.deleteAt(0);
+        } catch (Exception exception) {
+            return null;
+        }
+    }
+
+    public Node<T> peek() {
+        return this.linkedList.getNodeAt(0);
+    }
+
+    public boolean isEmpty() {
+        return this.linkedList.size() == 0;
+    }
 }

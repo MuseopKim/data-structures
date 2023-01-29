65cfa563576e1e840cb5fbee21b8b8782cebce6e
Museop <museopkim0214@gmail.com>
2023-01-29 23:49:02 +0900

    feat: queue

--- rewind/src/main/java/queue/Queue.java
+++ rewind/src/main/java/queue/Queue.java
@@ -1,2 +1,29 @@
-package queue;public class Queue {
+package queue;
+
+import doublylinkedlist.DoublyLinkedList;
+import doublylinkedlist.DoublyLinkedList.Node;
+
+public class Queue<T> {
+
+    private DoublyLinkedList<T> linkedList = new DoublyLinkedList<>();
+
+    public void enqueue(T data) {
+        this.linkedList.insertAt(0, data);
+    }
+
+    public Node<T> dequeue(T data) {
+        try {
+            return this.linkedList.deleteLast();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public Node<T> front() {
+        return this.linkedList.getLast();
+    }
+
+    public boolean isEmpty() {
+        return this.linkedList.size() == 0;
+    }
 }

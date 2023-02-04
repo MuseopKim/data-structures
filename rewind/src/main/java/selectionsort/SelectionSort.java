112082a6ed52feb07bb54c07a50fd8ebf649f394
Museop <museopkim0214@gmail.com>
2023-02-04 22:42:45 +0900

    feat: selection sort

--- rewind/src/main/java/selectionsort/SelectionSort.java
+++ rewind/src/main/java/selectionsort/SelectionSort.java
@@ -1,2 +1,31 @@
-package selectionsort;public class SelectionSort {
+package selectionsort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static int[] sort(int[] array) {
+        int[] copy = Arrays.copyOf(array, array.length);
+
+        for (int i = 0; i < copy.length - 1; i++) {
+            for (int j = i + 1; j < copy.length; j++) {
+                if (copy[i] > copy[j]) {
+                    int temp = copy[i];
+                    copy[i] = copy[j];
+                    copy[j] = temp;
+                }
+            }
+        }
+
+        return copy;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{2, 3, 1, 4, 5, 7, 6};
+        int[] sorted = SelectionSort.sort(array);
+
+        for (int i : sorted) {
+            System.out.println(i);
+        }
+    }
 }
